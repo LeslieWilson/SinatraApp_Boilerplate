331b55c3aa25fdfd435fb4baf37ea8cb1410c681
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-11-29 12:47:42 -0500

    welcome

---
+++ medium_example/kickball-site/server.rb
@@ -0,0 +1,7 @@
+require "sinatra"
+require "pry" if development? || test?
+require "sinatra/reloader" if development?
+require_relative "models/league"
+
+
+set :bind, '0.0.0.0'  # bind to all interfaces
