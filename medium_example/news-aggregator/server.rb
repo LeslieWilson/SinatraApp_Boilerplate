554a6350124084d726913ea21c47ae37f422f047
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-12-25 16:03:16 -0500

    throw messages onto browser

--- medium_example/news-aggregator/server.rb
+++ medium_example/news-aggregator/server.rb
@@ -8,24 +8,26 @@ require 'csv'
 set :bind, '0.0.0.0'
 
 get "/articles" do
+    @articles = []
+    CSV.foreach("myfile.csv") do |line|
+        @articles << [line[0], line[1]]
+    end
     erb :home
-
 end
+
+
 
 get "/articles/new" do
     erb :articles
 end
-
-
-
 post "/articles" do
 article_name = params["article_name"]
 article_description = params["article_description"]
-
 CSV.open("myfile.csv", "a") do |csv|
-  csv << ["title:" + article_name]
-  csv << ["description:" + article_description]
+  csv << ["title:" + article_name,"description:" + article_description ]
+
 end
 
 redirect  "/articles"
+
 end
