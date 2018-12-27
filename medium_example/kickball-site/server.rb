e64364c3315b2b9e86b397890c21512cbfc9a8e3
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-12-27 17:40:31 -0500

    err messages made

--- medium_example/kickball-site/server.rb
+++
@@ -1,7 +0,0 @@
-require "sinatra"
-require "pry" if development? || test?
-require "sinatra/reloader" if development?
-require_relative "models/league"
-
-
-set :bind, '0.0.0.0'  # bind to all interfaces
