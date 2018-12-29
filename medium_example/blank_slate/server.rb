00b07f8832b981af86cdc50ae2b08d8a60bb06aa
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-12-29 16:35:25 -0500

    still trying to remake this thing

--- medium_example/blank_slate/server.rb
+++ medium_example/blank_slate/server.rb
@@ -6,19 +6,20 @@ set :bind, '0.0.0.0'
 
 get "/information" do
     @information = []
-    CSV.foreach("myfile.csv",  headers:true) do |line|
-        @information << item["name"]
+    CSV.foreach("myfile.csv") do |line|
+        @information << line
     end
+
     erb :information
     end
 
 
 post "/information" do
-info_name = params["info_name"]
-info_description = params["info_description"]
+@info_name = params[:info_name]
+@info_description = params[:info_description]
 
 CSV.open("myfile.csv", "a") do |csv|
-  csv << [info_name]
+  csv << [:info_name]
     end
-redirect  "/information"
+redirect "/information"
 end
